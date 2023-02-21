5ef95bf05463e11f4ed5db22b6bc7e19756e8b9f
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-02-21 09:40:03 +0530

    Create add-plus-minus.py

---
+++ add-plus-minus.py
@@ -0,0 +1,16 @@
+for i in range(int(input())): 
+    n = int(input())
+ 
+    s = input()
+    sum = s[0] == '1'
+ 
+    for i in range(1, n):
+        if s[i] == '0' :
+            print('+', end = "")
+        elif sum:
+            print('-', end = "")
+            sum = 0
+        else :
+            print('+', end = "")
+            sum = 1
+    print()
