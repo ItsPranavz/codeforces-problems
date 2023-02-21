b303da6353ae33d8cd2fbdcdadbf09f3d4b5a89e
ItsPranavz <119229843+ItsPranavz@users.noreply.github.com>
2023-02-21 09:45:02 +0530

    Update add-plus-minus.py

--- 800/add-plus-minus.py
+++ 800/add-plus-minus.py
@@ -1,9 +1,7 @@
 for i in range(int(input())): 
     n = int(input())
- 
     s = input()
     sum = s[0] == '1'
- 
     for i in range(1, n):
         if s[i] == '0' :
             print('+', end = "")
